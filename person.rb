acbc5e769ede0e6a9da92133e4bf145ded26f2b8
david63011 <david630@gmail.com>
2023-01-30 16:44:53 -0300

    add classes

---
+++ person.rb
@@ -0,0 +1,30 @@
+class Person
+    attr_accessor :name, :age
+    attr_reader :id
+
+    def initialize(age, name: 'Unknown', parent_permission: true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    def can_use_services?
+        is_of_age || @parent_permission
+    end
+
+    
+    def is_of_age
+        @age >= 18
+    end
+
+  end
+
+  person1 = Person.new(13, name: 'jex', parent_permission: false)
+  puts person1.name
+  puts person1.age
+  puts person1.id
+  puts person1.can_use_services?
+
+
+
---
+++ student.rb
@@ -0,0 +1,19 @@
+require_relative 'person'
+class Student < Person
+    attr_accessor :classroom
+    def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+        super(age, name: name, parent_permission: parent_permission)
+        @classroom = classroom
+    end
+
+    def play_hooky
+        '¯\(ツ)/¯'
+    end
+   end
+
+   student1 = Student.new(13, name: 'Petesz', parent_permission: false, classroom: 'Math')
+   puts student1.name
+   puts student1.age
+   puts student1.can_use_services?
+   puts student1.play_hooky
+   puts student1.classroom
