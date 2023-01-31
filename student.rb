dc7d45c7e036cce1d8cc51ebc7c6546146ef01c6
david63011 <david630@gmail.com>
2023-01-31 10:36:32 -0300

    fix lintors

--- student.rb
+++ student.rb
@@ -1,19 +1,20 @@
 require_relative 'person'
 class Student < Person
-    attr_accessor :classroom
-    def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
-        super(age, name: name, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  attr_accessor :classroom
 
-    def play_hooky
-        '¯\(ツ)/¯'
-    end
-   end
+  def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-   student1 = Student.new(13, name: 'Petesz', parent_permission: false, classroom: 'Math')
-   puts student1.name
-   puts student1.age
-   puts student1.can_use_services?
-   puts student1.play_hooky
-   puts student1.classroom
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
+
+student1 = Student.new(13, name: 'Petesz', parent_permission: false, classroom: 'Math')
+puts student1.name
+puts student1.age
+puts student1.can_use_services?
+puts student1.play_hooky
+puts student1.classroom
--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,16 @@
 require_relative 'person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(age, specialization: 'Unknown', name: 'Unknown', parent_permission: true)
-        super(age, name: name, parent_permission: parent_permission)
-        @specialization = specialization
-    end
+  def initialize(age, specialization: 'Unknown', name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
 
 teacher1 = Teacher.new(87, name: 'Petesz', parent_permission: false, specialization: 'Math')
