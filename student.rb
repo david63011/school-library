76ce758669bc9ac3784e4eb6dfb07f2b8e4c5b04
david63011 <david630@gmail.com>
2023-02-01 14:56:33 -0300

    lintor changes

--- student.rb
+++ student.rb
@@ -2,25 +2,18 @@ require_relative 'person'
 require_relative 'classroom'
 
 class Student < Person
-  attr_reader :classroom 
+  attr_reader :classroom
 
-  def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
-  
-  def classroom = (classroom)
+
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
-
-student1 = Student.new(25, name: 'Petesz', parent_permission: false, classroom: 'Math')
-puts student1.name
-puts student1.age
-puts student1.can_use_services?
-puts student1.play_hooky
-puts student1.classroom
