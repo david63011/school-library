720cb89a94cd4d3c507c6e470ffca4c97b2b3d74
david63011 <david630@gmail.com>
2023-02-01 14:49:06 -0300

    add assocs

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 require_relative 'nameable'
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
@@ -9,6 +9,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -21,6 +22,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 end
 
--- student.rb
+++ student.rb
@@ -1,14 +1,20 @@
 require_relative 'person'
+require_relative 'classroom'
+
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom 
 
   def initialize(age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
     super(age, name: name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+  
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
