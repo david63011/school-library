ea9c1ddde846250f22d87d3f35161ab639006493
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2023-02-07 23:15:56 +0400

    AD BOOK

--- handler.rb
+++ handler.rb
@@ -38,6 +38,10 @@ module Handler
   #Store Books
   def store_books(app)
     array = []
+    array1 = read_json(BOOK_FILE)
+    array1.each do |book|
+      array.push(book)
+    end
     app.books.each do |book|
         array.push(title: book.title, author: book.author)
     end
@@ -48,7 +52,7 @@ module Handler
   def read_books
     array = read_json(BOOK_FILE)
     array.each do |book|
-      puts "Title: \"#{book.title}\", Author: #{book.author}"
+      puts "Title: \"#{book['title']}\", Author: #{book['author']}"
     end
   end
 
@@ -58,12 +62,12 @@ module Handler
     app.people.each do |person|
         array.push(class: person.class, name: person.name, age: person.age, id: person.id)
     end
-    write_json(array, @people_file)
+    write_json(array, PEOPLE_FILE)
   end
 
   #Read people
   def read_people
-    array = read_json(@people_file)
+    array = read_json(PEOPLE_FILE)
     array.each do |person|
       if person['class'] == 'Student'
           @people.push(Student.new(person['name'], person['age'], person['id']))
@@ -79,12 +83,12 @@ module Handler
     app.rentals.each do |rental|
         array.push(date: rental.date, person: rental.person, book: rental.book)
     end
-    write_json(array, @rental_file)
+    write_json(array, RENTAL_FILE)
   end
 
   #read rentals
   def read_rentals
-    array = read_json(@rental_file)
+    array = read_json(RENTAL_FILE)
     array.each do |rental|
         @rentals.push(Rental.new(rental['date'], rental['person'], rental['book']))
     end
