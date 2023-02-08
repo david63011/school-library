f7e2b9b6f931ad5248cff858bce012728a5b856f
david63011 <david630@gmail.com>
2023-02-08 16:22:33 -0300

    add new test

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,8 @@
+require_relative './spec_helper'
+
+describe Nameable do
+    nameable = Nameable.new
+    it 'should have the method correct the name' do
+        expect(nameable).to respond_to(:correct_name)
+    end
+    end
