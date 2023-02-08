6713fb097058d4adfc6903f3d891303596b745ff
david63011 <david630@gmail.com>
2023-02-08 16:25:53 -0300

    lintors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,24 +1,24 @@
-require 'spec_helper.rb'
+require 'spec_helper'
 
 describe Book do
-        book = Book.new('The Alchemist', 'Paulo Coelho')
-    it 'should be an instance of Book' do
-        expect(book).to be_an_instance_of(Book)
-    end
-    it 'should have a title' do
-        expect(book.title).to eql('The Alchemist')
-    end
+  book = Book.new('The Alchemist', 'Paulo Coelho')
+  it 'should be an instance of Book' do
+    expect(book).to be_an_instance_of(Book)
+  end
+  it 'should have a title' do
+    expect(book.title).to eql('The Alchemist')
+  end
   it 'should have an author' do
-        expect(book.author).to eql('Paulo Coelho')
-    end
-    it 'should have an empty array of rentals' do
-        expect(book.rentals).to eql([])
-    end
+    expect(book.author).to eql('Paulo Coelho')
+  end
+  it 'should have an empty array of rentals' do
+    expect(book.rentals).to eql([])
+  end
 
-    describe '#add_rental' do
-        it 'should add a rental to the book' do
-            book.add_rental('person', 'date')
-            expect(book.rentals).to eql(['person', 'date', book])
-        end
+  describe '#add_rental' do
+    it 'should add a rental to the book' do
+      book.add_rental('person', 'date')
+      expect(book.rentals).to eql(['person', 'date', book])
     end
-    end
+  end
+end
